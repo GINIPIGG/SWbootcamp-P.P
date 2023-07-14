46280b5b51ee4758e83175da30b68c06080ea1f5
BOOK-DLJSUO8O2N\kmkim <kmkim550126@naver.com>
2023-07-14 10:34:00 +0900

    Project Upload

---
+++ Assets/PinController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinController : MonoBehaviour
+{
+    public Rigidbody2D Pin;
+    public float pinspeed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
